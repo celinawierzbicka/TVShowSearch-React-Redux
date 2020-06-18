a1d39299c56de06ed98ab6c52e0c08a6ae375471
Celina Wierzbicka <celinawierzbicka@gmail.com>
2020-06-18 17:51:53 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import tvmaze from '../apis/tvmaze';
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>App</div>
+        )
+    }
+}
+
+export default App;
